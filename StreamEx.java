a7edb7496b585057833bb42a066a5fe0bc825de6
Preeti Dantkale <dantkalepreeti29@gmail.com>
2022-08-09 12:22:07 +0530

    Updated Display method to display data only

--- StreamEx.java
+++ StreamEx.java
@@ -1,21 +1,24 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
 @FunctionalInterface
 interface Print{
-    void display();
+    void display(List l);
 }
 public class StreamEx implements Print {
-    public void display()
+    public void display(List filteredList)
     {
+        filteredList.forEach(System.out::println);
+    }
+    public static void main(String[] args)
+    {
+        StreamEx s = new StreamEx();
         List<Integer> l = new ArrayList<Integer>();
         l.add(20);
         l.add(56);
         l.add(19);
         l.add(15);
-        l.stream().filter(n -> n > 50).forEach(System.out::println);
-    }
-    public static void main(String[] args)
-    {
-        StreamEx s = new StreamEx();
-        s.display();
+        List filteredList = l.stream().filter(n -> n > 50).collect(Collectors.toList());
+        s.display(filteredList);
     }
 }
